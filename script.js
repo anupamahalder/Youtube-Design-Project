2df78aef1adf90377509954886060cb65c2584b5
Anupama_Halder <anupamahalder2022@gmail.com>
2023-09-03 10:57:25 +0530

    Added time of published in the video thumbnail

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const handleCategory = async() =>{
         const div = document.createElement('div');
         div.innerHTML = `
         <a class="tab" onclick="handleLoadCategories('${category_item.category_id}')">
-            <button class="p-2 text-gray-700 rounded-md bg-gray-300 hover:bg-red-600 hover:text-white">${category_item.category}</button>
+            <button class="p-2 redBtn text-gray-700 rounded-md bg-gray-300 hover:bg-red-600 hover:text-white">${category_item.category}</button>
         </a> 
         `;
         tabContainer.appendChild(div);
@@ -27,6 +27,7 @@ const handleLoadCategories = async(categoryId)=>{
     const noDataContainer = document.getElementById('no-data-div');
     //clear the div
     cardContainer.innerHTML = '';
+    noDataContainer.innerHTML = "";
     if(data.status == false){
         const div = document.createElement('div');
         div.innerHTML=`
@@ -44,13 +45,15 @@ const handleLoadCategories = async(categoryId)=>{
         noDataContainer.appendChild(div);
         return;
     }
-    noDataContainer.innerHTML = "";
     data.data?.forEach((video)=>{
         const div = document.createElement('div');
         div.innerHTML = `
         <div class="w-72 bg-base-100 mx-auto py-3">
             <div class="h-44 py-4">
                 <img class="h-full w-full object-cover rounded-md" src=${video?.thumbnail} alt="thumbnail" />
+                <div class="flex justify-end -mt-8 p-2">
+                    <p class="bg-gray-800 rounded-xl inline-block text-white text-xs px-2">${video.others.posted_date?changeSeconds(video.others.posted_date):""}</p>
+                </div>
             </div>
             <div class="flex gap-2 text-center">
                 <div class="avatar">
@@ -74,6 +77,42 @@ const handleLoadCategories = async(categoryId)=>{
         cardContainer.appendChild(div);
     });
 }
-
+function changeSeconds(seconds){
+    const sec = parseInt(seconds);
+    let mins = sec/60;
+    let hours = Math.floor(mins/60);
+    let day = 0;
+    if(hours>24){
+        let rem_hours = Math.floor(hours%24);
+        day = Math.floor(hours/24);
+        hours = rem_hours;
+        console.log("days: ",day);
+    }
+    let years = 0;
+    if(day>365){
+        let rem_days = Math.floor(day%365);
+        years = Math.floor(rem_days/365);
+        day = rem_days;
+        console.log("years: ",years);
+    }
+    let rem_mins = Math.floor(mins%60);
+    let time = "";
+    if(years!= 0 && day !=0 && hours !== 0 && rem_mins !== 0){
+        time = `${years} year ${day} day ${hours} hrs ${rem_mins} mins ago`;
+    }
+    if(day !=0 && hours !== 0 && rem_mins !== 0){
+        time = `${day} day ${hours} hrs ${rem_mins} mins ago`;
+    }
+    else if(hours !== 0 && rem_mins !== 0){
+        time = `${hours} hrs ${rem_mins} mins ago`;
+    }
+    else if(hours === 0 && rem_mins !== 0){
+        time = `${rem_mins} mins ago`;
+    }
+    else if(hours !== 0 && rem_mins === 0){
+        time = `${hours} hrs ago`;
+    }
+    return time;
+}
 handleCategory();
 // handleLoadCategories("1000");
