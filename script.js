9a78e6ef73d6d0fce54c95d1e86ccb5750816908
Anupama_Halder <anupamahalder2022@gmail.com>
2023-09-03 09:01:37 +0530

    Added data not found image when no data is fetched from API

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const handleCategory = async() =>{
         const div = document.createElement('div');
         div.innerHTML = `
         <a class="tab" onclick="handleLoadCategories('${category_item.category_id}')">
-            <button class="py-2 px-3 text-gray-700 rounded-md bg-gray-300 hover:bg-red-600 hover:text-white">${category_item.category}</button>
+            <button class="p-2 text-gray-700 rounded-md bg-gray-300 hover:bg-red-600 hover:text-white">${category_item.category}</button>
         </a> 
         `;
         tabContainer.appendChild(div);
@@ -20,16 +20,35 @@ const handleCategory = async() =>{
 const handleLoadCategories = async(categoryId)=>{
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
     const data = await response.json();
-    // console.log(data.data);
+    // console.log(data);
     //get the card container
     const cardContainer = document.getElementById('card-container');
+    //get the data not found div
+    const noDataContainer = document.getElementById('no-data-div');
     //clear the div
     cardContainer.innerHTML = '';
+    if(data.status == false){
+        const div = document.createElement('div');
+        div.innerHTML=`
+            <div class="w-full h-full flex justify-center text-center mx-auto">
+                <div class="w-full">
+                    <div class="w-full my-6 justify-center">
+                        <img class="mx-auto" src="./images/Icon.png" alt="data not found!">
+                    </div>
+                    <div class="py-5 w-60 border border-red-500 mx-auto">
+                        <h1 class="p-5">Oops!! Sorry, There is no content here</h1>
+                    </div>
+                </div>
+            </div>
+        `;
+        noDataContainer.appendChild(div);
+        return;
+    }
+    noDataContainer.innerHTML = "";
     data.data?.forEach((video)=>{
-        console.log(video);
         const div = document.createElement('div');
         div.innerHTML = `
-        <div class="w-72 bg-base-100 mx-auto">
+        <div class="w-72 bg-base-100 mx-auto py-3">
             <div class="h-44 py-4">
                 <img class="h-full w-full object-cover rounded-md" src=${video?.thumbnail} alt="thumbnail" />
             </div>
@@ -43,9 +62,9 @@ const handleLoadCategories = async(categoryId)=>{
                     <h1 class="text-center font-bold">${video?.title}</h1>
                 </div>
             </div>
-            <div class="pl-12 text-gray-500 text-xs">
+            <div class="pl-12 pt-1 text-gray-500 text-xs">
                 <p class="pb-1">${video?.authors[0]?.profile_name}</p>
-                <p>${video?.others?.views} views</p>
+                <p>${video?.others?.views}</p>
             </div>
         </div>
         `;
